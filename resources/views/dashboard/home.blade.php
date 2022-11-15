3654c32f78a443f07876c0d9fa507be32ba26835
b-lema <lema@adanianlabs.io>
2022-11-15 23:40:43 +0300

    fromHome

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -378,11 +378,11 @@
     <footer>
         <div class="footer clearfix mb-0 text-muted">
             <div class="float-start">
-                <p>2021 &copy; Soeng Souy</p>
+                <p>2022 &copy; Lema Brian</p>
             </div>
             <div class="float-end">
                 <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                href="http://souysoeng.com">Soeng Souy</a></p>
+                href="">Lema Brian</a></p>
             </div>
         </div>
     </footer>
