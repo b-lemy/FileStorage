4d883660182e41d1d5e4f57028bd0a1e63d332bb
b-lemy <lemabrian1234@gmail.com>
2022-11-18 13:37:23 +0300

    password page office

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -42,10 +42,8 @@
                     </div>
                 </li>
 
-
-
                 @if (Auth::user()->role_name=='Admin')
-                    <li class="sidebar-title">Page &amp; Controller</li>
+                    <li class="sidebar-title">Page</li>
                     <li class="sidebar-item  has-sub {{set_active(['userManagement','activity/log','activity/login/logout'])}}">
                         <a href="#" class='sidebar-link'>
                             <i class="bi bi-hexagon-fill"></i>
@@ -65,44 +63,44 @@
                     </li>
                 @endif
 
-{{--                <li class="sidebar-title">Forms &amp; Tables</li>--}}
-{{--                <li class="sidebar-item  has-sub {{set_active(['form/staff/new','form/checkbox/new'])}}">--}}
-{{--                    <a href="#" class='sidebar-link'>--}}
-{{--                        <i class="bi bi-file-earmark-medical-fill"></i>--}}
-{{--                        <span>Form Elements</span>--}}
-{{--                    </a>--}}
-{{--                    <ul class="submenu {{set_active(['form/staff/new','form/checkbox/new'])}}">--}}
-{{--                        <li class="submenu-item {{set_active(['form/staff/new'])}}">--}}
-{{--                            <a href="{{ route('form/staff/new') }}">Form Input</a>--}}
-{{--                        </li>--}}
-{{--                         <li class="submenu-item {{set_active(['form/checkbox/new'])}}">--}}
-{{--                            <a href="{{ route('form/checkbox/new') }}">Form Checkbox</a>--}}
-{{--                        </li>--}}
-{{--                    </ul>--}}
-{{--                </li>--}}
-{{--                <li class="sidebar-item  has-sub {{set_active(['form/view/detail'])}}">--}}
-{{--                    <a href="#" class='sidebar-link'>--}}
-{{--                        <i class="bi bi-grid-1x2-fill"></i>--}}
-{{--                        <span>View Record</span>--}}
-{{--                    </a>--}}
-{{--                    <ul class="submenu {{set_active(['form/view/detail'])}}">--}}
-{{--                        <li class="submenu-item {{set_active(['form/view/detail'])}}">--}}
-{{--                            <a href="{{ route('form/view/detail') }}">View Detail</a>--}}
-{{--                        </li>--}}
-{{--                    </ul>--}}
-{{--                </li>--}}
-{{--                <li class="sidebar-item">--}}
-{{--                    <a href="{{ route('lock_screen') }}" class='sidebar-link'>--}}
-{{--                        <i class="bi bi-lock-fill"></i>--}}
-{{--                        <span>Lock Screen</span>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li class="sidebar-item">--}}
-{{--                    <a href="{{ route('logout') }}" class='sidebar-link'>--}}
-{{--                        <i class="bi bi-box-arrow-right"></i>--}}
-{{--                        <span>Log Out</span>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
+                <li class="sidebar-title">Forms &amp; Tables</li>
+                <li class="sidebar-item  has-sub {{set_active(['form/staff/new','form/checkbox/new'])}}">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-file-earmark-medical-fill"></i>
+                        <span>Form Elements</span>
+                    </a>
+                    <ul class="submenu {{set_active(['form/staff/new','form/checkbox/new'])}}">
+                        <li class="submenu-item {{set_active(['form/staff/new'])}}">
+                            <a href="{{ route('form/staff/new') }}">Form Input</a>
+                        </li>
+                         <li class="submenu-item {{set_active(['form/checkbox/new'])}}">
+                            <a href="{{ route('form/checkbox/new') }}">Form Checkbox</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item  has-sub {{set_active(['form/view/detail'])}}">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-grid-1x2-fill"></i>
+                        <span>View Record</span>
+                    </a>
+                    <ul class="submenu {{set_active(['form/view/detail'])}}">
+                        <li class="submenu-item {{set_active(['form/view/detail'])}}">
+                            <a href="{{ route('form/view/detail') }}">View Detail</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="{{ route('lock_screen') }}" class='sidebar-link'>
+                        <i class="bi bi-lock-fill"></i>
+                        <span>Lock Screen</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="{{ route('logout') }}" class='sidebar-link'>
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Log Out</span>
+                    </a>
+                </li>
             </ul>
         </div>
         <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
