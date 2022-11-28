7ab9ffffab520ce85a01b3dbefa5f2a6e7e15dc5
b-lemy <lemabrian1234@gmail.com>
2022-11-28 15:17:30 +0300

    settersandgetters

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -28,24 +29,28 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function firstname(): Attribute
+    {
+        return new Attribute(
+            //getters
+           get: fn($value) => ucwords($value),
+
+//            setters
+//           set: fn($value) => strtolower($value)
+        );
+    }
 
     public function file(){
         return $this->hasMany(File::class);
